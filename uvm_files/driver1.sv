dd065202ab0816f7e62d3ee81fbd307d774e26e9
mitshine <mitesh.khadgi2018@gmail.com>
2025-02-01 19:26:11 +0530

    Add files via upload

--- uvm_files/driver1.sv
+++ uvm_files/driver1.sv
@@ -1,4 +1,15 @@
 
 class driver1 extends uvm_driver;
 
+rand bit  in;
+
+virtual function run_phase()
+
+endfunction
+
+virtual function void build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  s0 = uvm_sequencer#(Item)::type_id::create("s0", this);
+endfunction
+
 endclass
