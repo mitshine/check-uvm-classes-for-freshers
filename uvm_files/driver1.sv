7097ca9f964c1653b872dba6a88db56b95da6070
mitshine <mitesh.khadgi2018@gmail.com>
2025-02-01 20:59:33 +0530

    Add files via upload

--- uvm_files/driver1.sv
+++ uvm_files/driver1.sv
@@ -3,13 +3,28 @@ class driver1 extends uvm_driver;
 
 rand bit  in;
 
-virtual function run_phase()
-
-endfunction
+p_sequencer sqr1;
+m_sequencer sqr2;
 
 virtual function void build_phase(uvm_phase phase);
   super.build_phase(phase);
   s0 = uvm_sequencer#(Item)::type_id::create("s0", this);
 endfunction
+
+virtual function run_phase1();
+  phase.raise_objection();
+  call_randomize(txn);
+  phase.drop_objection();
+endfuntion
+
+virtual function run_phase();
+  start_item();
+  assert.randomize(txn);
+  finish_item();
+endfuntion
+
+virtual task task_name();
+
+endtask
 
 endclass
